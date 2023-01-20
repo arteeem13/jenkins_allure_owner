9ec333cd7d2e167b7fa3a3fbabccd5171c3e9951
qa-artem <artem1305@yandex.ru>
2023-01-20 22:42:19 +0300

    added owner

--- src/test/java/com/andreev/tests/TestBase.java
+++ src/test/java/com/andreev/tests/TestBase.java
@@ -1,14 +1,19 @@
 package com.andreev.tests;
 
+import com.andreev.config.CredentialsConfig;
 import com.andreev.helpers.Attachments;
 import com.codeborne.selenide.Configuration;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
-    private static String browserName = System.getProperty("browser", "firefox");
-    private static String browserSize = System.getProperty("browserSize", "1920x1080");
+    private static final String browserName = System.getProperty("browser", "firefox");
+    private static final String browserSize = System.getProperty("browserSize", "1920x1080");
+    private static final String browserVersion = System.getProperty("browserVersion", "90");
+    private static final String url = System.getProperty("url", "selenoid.autotests.cloud/wd/hub");
+    public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
 
         @BeforeAll
         public static void beforeAllTests(){
@@ -16,11 +21,11 @@ public class TestBase {
             capabilities.setCapability("enableVNC", true);
             capabilities.setCapability("enableVideo", true);
             Configuration.browserCapabilities = capabilities;
-            System.out.println(browserName);
             Configuration.browser = browserName;
+            Configuration.browserVersion = browserVersion;
             Configuration.browserSize = browserSize;
             Configuration.baseUrl = "https://demoqa.com";
-            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+            Configuration.remote = "https://" + credentials.login() + ":" + credentials.password() + "@" + url;
     }
 
     @AfterEach
